29809c0593fb8aade377d5291f5fc1b8c25674d9
barikq <yar.barik@gmail.com>
2021-03-30 14:07:35 +0300

    Offer-image size

--- src/script/meter.js
+++ src/script/meter.js
@@ -2,29 +2,33 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let ratingAmount = parseInt(document.querySelector('#ratingValue').innerHTML);
   let meterArrow = document.querySelector('.meter-arrow');
   let angle = 0;
-  const angleOffset = 90 + 18;
+  const angleOffset = 90 + 10;
   const maxAngle = 180;
   const blocksAmount = 5;
-
-  let block = 0;
+  const veryLowOffset = -77;
+  const lowOffset = -42;
+  const averageOffset = 0;
+  const hightOffset = 42;
+  const veryHightOffset = 77;
 
   if (ratingAmount >= 300 && ratingAmount <= 400) {
-    block = 1;
+    angle = veryLowOffset;
   }
   if (ratingAmount >= 401 && ratingAmount <= 500) {
-    block = 2;
+    angle = lowOffset;
   }
   if (ratingAmount >= 501 && ratingAmount <= 600) {
-    block = 3;
+    angle = averageOffset;
   }
   if (ratingAmount >= 601 && ratingAmount <= 700) {
-    block = 4;
+    angle = hightOffset;
   }
   if (ratingAmount >= 701 && ratingAmount <= 850) {
-    block = 5;
+    angle = veryHightOffset;
   }
 
-  angle = block * maxAngle / blocksAmount - angleOffset;
+  // angle = block * maxAngle / blocksAmount - 90;
+  // console.log(angle);
 
-  meterArrow.style.transform = `translate(-50%, 8px) rotate(${angle}deg)`;
+  meterArrow.style.transform = `translate(-50%, 0px) rotate(${angle}deg)`;
 });
